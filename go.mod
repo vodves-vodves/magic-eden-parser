f84e8295a5f0d6436a42262324e21d1a6157cc9c
piplist <khaziev_irekk@mail.ru>
2022-09-11 01:24:24 +0500

    change 1

--- go.mod
+++ go.mod
@@ -7,8 +7,12 @@ require (
 	github.com/EDDYCJY/fake-useragent v0.2.0 // indirect
 	github.com/PuerkitoBio/goquery v1.7.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/bwmarrin/discordgo v0.26.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/tidwall/gjson v1.14.3 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 )
--- main.go
+++ main.go
@@ -3,19 +3,57 @@ package main
 import (
 	"fmt"
 	cloudflarebp "github.com/DaRealFreak/cloudflare-bp-go"
+	"github.com/bwmarrin/discordgo"
 	"github.com/tidwall/gjson"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
+const token = "MTAxODI0OTE0NjMxNjEwNzc4Ng.GIWUSj.wYI8Caw2UPq43Vyz0bCcQd49L7FZ2-hKIDuy-E"
+
+var botId string
+
 func main() {
+	//start()
 	floorCollections("utility_ape")
 	//popularCollections()
 }
+func start() {
+	goBot, err := discordgo.New("Bot" + token)
+	if err != nil {
+		fmt.Println("1", err)
+		return
+	}
+	u, err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	botId = u.ID
+	goBot.AddHandler(messageHandler)
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("bot started")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == botId {
+		return
+	}
+	result := floorCollections("utility_ape")
+	if m.Content == "!test" {
+		for _, item := range result {
+			s.ChannelMessageSend(m.ChannelID, item)
+		}
+	}
+}
 
 //Выводит флор определенной коллекции
-func floorCollections(collectionSymbol string) {
+func floorCollections(collectionSymbol string) []string {
 	var result []string
 	url := "https://api-mainnet.magiceden.io/rpc/getListedNFTsByQueryLite"
 	query := map[string]string{
@@ -27,14 +65,17 @@ func floorCollections(collectionSymbol string) {
 		result = append(result, fmt.Sprintf("%s - %s $SOL - https://www.magiceden.io/item-details/%s\n", item.Get("title").String(), item.Get("price").String(), item.Get("mintAddress").String()))
 	}
 	fmt.Println(result)
+	return result
 }
 
 //Выводит популярные коллекции
 func popularCollections() {
+	var result []string
 	url := "https://api-mainnet.magiceden.io/popular_collections"
 	query := make(map[string]string)
-	result := sendRequest(url, query)
-	fmt.Println(result)
+	json := sendRequest(url, query)
+	value := gjson.Get(json, "collections")
+	fmt.Println(result, value)
 }
 
 //Отправка запроса
